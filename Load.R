7b82d618fb29969476775e07f624b43846b2572b
Sifan Liu <SLiu@brookings.edu>
2018-02-01 12:31:11 -0500

    change source file location

--- Load.R
+++ Load.R
@@ -7,7 +7,7 @@ library('data.table')
 setwd("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/01182018/Sifan")
 
 master <- read_xlsx("../Original data/GMM17 database v3.xlsx")
-group <- read.csv("GMM17 groups.csv")
+group <- read.csv("source/GMM17 groups.csv")
 
 MENA <- read_xlsx("../Original data/MENA database v2.xlsx")
 
