fa28b696d5d7b61f546363cd709ee8e790d018fc
fansi-sifan <SLiu@brookings.edu>
2018-04-20 17:58:21 -0400

    add MENA map

--- Load.R
+++ Load.R
@@ -1,12 +1,14 @@
 library('readxl')
 
 
-#master <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/GMM17 database v4.xlsx")
+# master <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/GMM17 database v4.xlsx")
+# MENA <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/01182018/Original data/MENA database v2.xlsx")
+# save(master, file = 'master.Rdata')
+
+
 group <- read.csv("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/01182018/Sifan/source/GMM17 groups.csv") %>% 
   select(country, region, incomegroup) %>% 
   unique
 
-#MENA <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/01182018/Original data/MENA database v2.xlsx")
-
-#China <- filter(master, Country == "China")
-#save(master, file = 'master.Rdata')
+worldmap <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/index/GMM17_index_50_50_bothperiods_coordinates.xlsx")
+MENAmap <- read_xlsx("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/pockets/GMM17MENA_country_metro_comparison_2014_2016.xlsx")
--- World Map.R
+++ World Map.R
@@ -30,7 +30,7 @@ ggplot() +
   geom_point(data = worldmap, aes(x = Longitude, y = Latitude, color = istop_q_14_16, size = `2016`), alpha = 0.8) + 
   scale_size_continuous(labels = scales::comma, name = "Nominal GDP(PPP), 2016") +
   scale_color_manual(values = c("#f46d43", "#1a9850"), labels = c("All others","Top quintile"), name = "Performance Status") +
-  geom_text(data = worldmap, aes(x = Longitude, y = Latitude, label = finalname),
+  geom_text(data = worldmap, aes(x = Longitude, y = Latitude, label = metrofinalname),
             check_overlap = TRUE, nudge_y = 1.5, color = "#636363", size = 2) +
   # geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = finalname), 
   #           nudge_y = 1.5, color = "#636363", size = 2) +
@@ -51,6 +51,26 @@ ggsave("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/plots/worl
 
 #ggsave("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/plots/worldmap.pdf", width = 20, height = 12)  
 
+# MENA pockets of growth --------------------------------------------------
+
+ggplot() +
+  geom_polygon(data = map.world, aes(x = long, y = lat, group = group), fill = "#f0f0f0", color = "white") +
+  geom_point(data = MENAmap, aes(x = Longitude, y = Latitude, color = as.factor(pockets_growth2014_2016), size = gdpppp_2016), alpha = 0.8) + 
+  scale_size_continuous(labels = scales::comma, name = "Nominal GDP(PPP), 2016") +
+  scale_color_manual(values = c("#b2182b", "#2166ac"), labels = c("All others","Pockets of growth"), name = "Growth Status") +
+  geom_text_repel(data = MENAmap, aes(x = Longitude, y = Latitude, label = metro), 
+            nudge_y = 0.5, color = "#636363", size = 2) +
+  coord_quickmap(xlim = c(-12,62), ylim = c(10, 40)) +
+  
+  theme(panel.background = element_blank(),
+        plot.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank())
+
+ggsave("V:/MetroMonitor/Global Monitor/Global Monitor V/Data/04132018/plots/MENA.png", width = 10, height = 6)  
+
 
 # China -------------------------------------------------------------------
 
@@ -59,7 +79,7 @@ ggplot() +
   geom_point(data = worldmap, aes(x = Longitude, y = Latitude, color = istop_q_14_16, size = `2016`), alpha = 0.8) + 
   scale_size_continuous(labels = scales::comma, name = "Nominal GDP(PPP), 2016") +
   scale_color_manual(values = c("#f46d43", "#1a9850"), labels = c("All others","Top quintile"), name = element_blank()) +
-  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = finalname), 
+  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = metrofinalname), 
             nudge_y = 0.5, color = "#636363", size = 2) +
   coord_map("mercator",xlim = c(100,145), ylim = c(20, 45)) +
   
@@ -79,7 +99,7 @@ ggplot() +
   geom_point(data = worldmap, aes(x = Longitude, y = Latitude, color = istop_q_14_16, size = `2016`), alpha = 0.8) + 
   scale_size_continuous(labels = scales::comma, name = "Nominal GDP(PPP), 2016") +
   scale_color_manual(values = c("#f46d43", "#1a9850"), labels = c("All others","Top quintile"), name = element_blank()) +
-  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = finalname), 
+  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = metrofinalname), 
             nudge_y = 0.5, color = "#636363", size = 3) +
   coord_quickmap(xlim = c(-125,-60), ylim = c(18, 52)) +
   
@@ -99,7 +119,7 @@ ggplot() +
   geom_point(data = worldmap, aes(x = Longitude, y = Latitude, color = istop_q_14_16, size = `2016`), alpha = 0.8) + 
   scale_size_continuous(labels = scales::comma, name = "Nominal GDP(PPP), 2016") +
   scale_color_manual(values = c("#f46d43", "#1a9850"), labels = c("All others","Top quintile"), name = element_blank()) +
-  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = finalname), 
+  geom_text(data = filter(worldmap,istop_q_14_16 == 1), aes(x = Longitude, y = Latitude, label = metrofinalname), 
             nudge_y = 0.5, color = "#636363", size = 3) +
   coord_quickmap(xlim = c(-10,25), ylim = c(40, 60)) +
   
